9fba55917bbea4faf3031608ba5e46133fa4baa9
Crystal Wen <wen00015@umn.edu>
2024-03-13 13:56:47 -0500

    Worked on coinToss, not finished

--- Project1/src/Election.java
+++ Project1/src/Election.java
@@ -1,12 +1,12 @@
-import java.io.File;
+// import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.random.*;
 
 public class Election{
     protected int totalVotes;
     protected int totalSeats;
     protected ArrayList<Party> parties;
-    protected ArrayList<Candidate> winners;
     protected int quota;
     protected Scanner fileReader;
 
@@ -25,8 +25,9 @@ public class Election{
 
     }
 
-    protected ArrayList<Party> coinToss() {
-        
+    protected ArrayList<Party> coinToss(ArrayList<Party> tiedParties) {
+        ArrayList<Party> winners= new ArrayList<>();
+        return winners;
     }
 
     protected void allocateSeats() {
