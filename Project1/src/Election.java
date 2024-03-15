9743a796d496c79d928ab73e5db642aea0bbd787
sawashu <sawamurasatoko@Shun-MacBook.local>
2024-03-15 16:43:23 -0500

    completed allocateSeats(), displayResults(), auditFile()

--- Project1/src/Election.java
+++ Project1/src/Election.java
@@ -9,6 +9,7 @@ public class Election{
     protected ArrayList<Party> parties;
     protected int quota;
     protected Scanner fileReader;
+    protected ArrayList<Candidate> winnerList;
 
     public Election(int totalVotes, int totalSeats, ArrayList<Party> parties, Scanner fileReader){
         this.totalVotes = totalVotes;
@@ -22,7 +23,6 @@ public class Election{
     }
 
     public void voteCounting() {
-
     }
 
     public ArrayList<Party> coinToss(ArrayList<Party> tiedParties) {
@@ -31,18 +31,56 @@ public class Election{
     }
 
     public void allocateSeats() {
-
+        int remainingSeats = this.totalSeats;
+        //divide the number of votes that each party gets by the quota.
+        for (Party party: parties) {
+            //seat allocation first round
+            int firstAllocatedSeats = party.getNumVotes() / this.quota;
+            party.setNumAllocatedSeats(firstAllocateSeats);
+            remainingSeats -= firstAllocatedSeats;
+            //update 
+        }
+        //Distribute remaining seats by comparing the largest remainder of votes 
+        //for each party in a round-robin fashion.
+        while (remainingSeats > 0){
+            int largestRemainingVotes = -1;
+            Party allocatedParty;
+            for (Party party: parties) {
+                //calculate remaining votes
+                int remainingVotes = party.getNumVotes() - this.quota * party.getNumAllocatedSeats();
+                if (remainingVotes > largestRemainingVotes) {
+                    largestRemainingVotes = remainingVotes;
+                    allocatedParty = party;
+                }
+            }
+            allocatedParty.setNumAllocatedSeats(allocatedParty.getNumAllocatedSeats() + 1);
+        }
     }
 
     public void findWinners() {
-
+        
     }
 
     public void displayResults() {
-
+        DisplayResults results = new DisplayResults(
+                "CPL", 
+                this.parties.size(), 
+                this.totalVotes, 
+                this.totalSeats,
+                this.winnerList,
+                );
     }
 
     public void auditFile() {
-        
+        Audit auditFile = new Audit(
+                "CPL",
+                this.parties.size();
+                this.totalVotes,
+                this.totalSeats,
+                this.winnerList
+                );
     }
+
 }
+
+
