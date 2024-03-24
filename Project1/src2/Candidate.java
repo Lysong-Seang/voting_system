eb36b02933621562d2cfd410cd917b1dd68a1a23
Crystal Wen <wen00015@umn.edu>
2024-03-24 17:44:17 -0500

    Added Fumi's comments to Candidate.java

--- Project1/src2/Candidate.java
+++ Project1/src2/Candidate.java
@@ -1,33 +1,49 @@
 package src2;
 
+/**
+ * @author Fumisato Teranishi
+ */
 public class Candidate {
+    /**
+     * Field descriptions of the Cadidate class
+     * @param name
+     * @param party
+     * @param numVotes
+     */
     private String name;
+    private String party;
     private int numVotes;
-    private String partyName;
 
-    public Candidate(String name, String partyName, int numVotes){
+    /**
+     * The constructor for Candidate
+     */
+    public Candidate(String name, String party, int numVotes) {
         this.name = name;
-        this.partyName = partyName;
+        this.party = party;
         this.numVotes = numVotes;
     }
 
+    /**
+     * The get and set methods below
+     */
+
+// Obtaining the Name
     public String getName() {
-        return name;
+        return this.name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getNumVotes() {
-        return numVotes;
-    }
-
-    public void setNumVotes(int numVotes) {
-        this.numVotes = numVotes;
-    }
-
+// Obtaining the Party
     public String getParty() {
-        return partyName;
+        return this.party;
+    }
+
+// Obtaining the number of votes
+    public int getNumVotes() {
+        return this.numVotes;
+    }
+
+// Setting the number of votes
+    public void setNumVotes(int voteNum) {
+        this.numVotes = voteNum;
     }
 }
