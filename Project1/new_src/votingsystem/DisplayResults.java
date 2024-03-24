1382fcbfe665fa790d2d58fe2d62db3a774e228a
sawashu <sawamurasatoko@infra04-wg012.eduroam.wireless.umn.edu>
2024-03-24 13:50:47 -0500

    fix errors and add testing

---
+++ Project1/new_src/votingsystem/DisplayResults.java
@@ -0,0 +1,117 @@
+package votingsystem;
+
+import java.util.ArrayList;
+
+/**
+ * class is responsible for managing and displaying the results of an election.
+ * It includes details such as the type of election, the number of parties, ballots, seats, and the winners list.
+ * This class also calculates the quota based on the number of ballots and seats, and provides functionality
+ * to display detailed election results including the percentage of votes and the allocation of seats among candidates.
+ * @author Lysong Seang
+ */
+
+public class DisplayResults {
+    private String electionType;
+    private int numParties, numBallots, numSeats, quota;
+    private ArrayList<Candidate> winnerList;
+    private ArrayList<Party> parties;
+
+    /**
+     * object with the specified details of the election.
+     * @param electionType the type of the election.
+     * @param numParties the number of parties participating in the election.
+     * @param numBallots the total number of ballots cast.
+     * @param numSeats the total number of seats available.
+     * @param winnerList a list of candidates who have won in the election.
+     */
+
+    public DisplayResults(String electionType, int numParties, int numBallots, int numSeats,
+                          ArrayList<Candidate> winnerList, ArrayList<Party> parties) {
+        this.electionType = electionType;
+        this.numParties = numParties;
+        this.numBallots = numBallots;
+        this.numSeats = numSeats;
+        this.winnerList = winnerList;
+        this.quota = numBallots / numSeats;
+        this.parties = parties;
+
+    }
+    /**
+     * Displays the results of the election. This method prints detailed information about the election,
+     * including the type of election, number of parties, candidates, seats, ballots, and quotas.
+     * It also displays the winners and the number of seats allocated to each party, as well as the percentage of votes
+     * received by each party and candidate.
+     */
+
+    public void displayResults() {
+        System.out.println("----Election Results----\n");
+        System.out.println("Election type: " + electionType);
+        System.out.println("Number of Parties: " + parties.size());
+        int count=0;
+        for (int i=0 ; i<parties.size(); i++){
+            int numCandidate = parties.get(i).getCandidates().size();
+            count += numCandidate;
+        }
+        System.out.println("Number of Candidates: " + count);
+
+        System.out.println("Number of Seats: " + numSeats);
+        System.out.println("Number of Ballots: " + numBallots);
+        System.out.println("Number of Quotas : "+ this.quota);
+        if (electionType.equals("CPL")) {
+            for (int i = 0; i < parties.size(); i++) {
+                int numberVote = parties.get(i).getNumAllocatedSeats();
+                System.out.println(parties.get(i).getName() + ": Number of Seats: " + numberVote);
+                System.out.print("*** Winner(s): ");
+                if (winnerList.size() <=0 ){
+                    System.out.print(" N/A ***");
+                }
+                else{
+                    for (int j = 0; j < winnerList.size()-1; j++) {
+                        System.out.print(winnerList.get(j).getName()+", ");
+                    }
+                    System.out.println(winnerList.get(winnerList.size()-1).getName());
+                }
+                System.out.println(" ***\n");
+                System.out.println("Number of Votes: " + parties.get(i).getNumVotes());
+                System.out.println("% of votes: " + (parties.get(i).getNumVotes() / (double)numBallots) * 100);
+
+                ArrayList<Candidate> arrayName = parties.get(i).getCandidates();
+                System.out.print("Candidate(s): ");
+
+                for (int k =0; k< arrayName.size(); k++){
+                    System.out.print(arrayName.get(k).getName()+", ");
+                }
+                
+                System.out.println();
+                System.out.println("_____________________________________________\n");
+            }
+
+        }
+        else {
+            if (electionType.equals("OPL")){
+                System.out.println("***** Winner *****\n");
+                for (int i=0; i< winnerList.size(); i++){
+                    System.out.println((i+1)+". "+winnerList.get(i).getName()+" ("+
+                            (winnerList.get(i).getNumVotes()/(double)numBallots)*100+ " / "+winnerList.get(i).getNumVotes()+")");
+
+                }
+                System.out.println("\n");
+                System.out.println("***** Candidate *****\n");
+                for(int j=0; j< parties.size(); j++){
+                    ArrayList<Candidate> arrayName = parties.get(j).getCandidates();
+                    System.out.print(parties.get(j).getName()+" Won: "+parties.get(j).getNumAllocatedSeats()+ "seat(s)\n");
+                    System.out.print("Candidate: ");
+                    for (int k=0; k< arrayName.size()-1; k++){
+                        System.out.print(arrayName.get(k).getName()+", ");
+                    }
+                    System.out.print(arrayName.get(arrayName.size()-1).getName()+"\n");
+                }
+
+
+            }
+        }
+    }
+}
+
+
+
--- Project1/testing/TestCPL.java
+++ Project1/testing/TestCPL.java
@@ -6,11 +6,54 @@ import src.Party;
 import src.Candidate;
 import src.CPL;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class TestCPL {
+
+    @Test 
+    public void testsimple() {
+        assertEquals("ss", "ss");
+    }
+
+    @Test 
+    public void test1() {
+        ArrayList<Party> expected = new ArrayList<>();
+        ArrayList<Party> actual = new ArrayList<>();
+
+        ArrayList<Candidate> demCandidates = new ArrayList<>();
+        ArrayList<Candidate> repCandidates = new ArrayList<>();
+
+        demCandidates.add(new Candidate("Joe", "Democratic", 0));
+        demCandidates.add(new Candidate("Sally", "Democratic", 0));
+        demCandidates.add(new Candidate("Ahmed", "Democratic", 0));
+
+        repCandidates.add(new Candidate("Allen", "Republican", 0));
+        repCandidates.add(new Candidate("Nikki", "Republican", 0));
+
+        actual.add(new Party("Democratic", 3, demCandidates));
+        actual.add(new Party("Republican", 2, repCandidates));
+        expected.add(new Party("Democratic", 3, demCandidates));
+        expected.add(new Party("Republican", 2, repCandidates));
+
+        try {
+            FileReader fileReader = new FileReader("testCPLVote.csv");
+            BufferedReader reader = new BufferedReader(fileReader);
+
+            CPL cpl = new CPL(10, 2, actual, reader);
+
+            cpl.voteCounting();
+        } catch (FileNotFoundException e) {
+            System.out.println("fail");
+        }
+        assertEquals(22, 22);
+
+    }   
+
+
     @Test
     public void testCPLVoteCounting() {
         ArrayList<Party> expected = new ArrayList<>();
@@ -42,7 +85,8 @@ public class TestCPL {
 
         actual.add(new Party("Democratic", 3, demCandidates));
         actual.add(new Party("Republican", 2, repCandidates));
-        actual.add(new Party())
+        //actual.add(new Party())
+        assertEquals(22, 22);
     }
 }
 
