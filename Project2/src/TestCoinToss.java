6834c21239f2a5442741997ba5a1f8fa137027d3
Crystal Wen <wen00015@umn.edu>
2024-04-09 17:02:52 -0500

    Updated TestCoinToss

--- Project2/src/TestCoinToss.java
+++ Project2/src/TestCoinToss.java
@@ -10,25 +10,20 @@ import java.util.ArrayList;
  * @author Crystal Wen
  */
 public class TestCoinToss {
-    public static void testCoinTossCPL() throws FileNotFoundException{
+    public static void testCoinToss() throws FileNotFoundException{
         ArrayList<Party> parties = new ArrayList<>();
         int totalVotes = 10000;
         int totalSeats = 3;
-        FileReader fileReader = new FileReader("testCPLVote.csv");
+        FileReader fileReader = new FileReader("testing/testCPLVote.csv");
         BufferedReader br = new BufferedReader(fileReader);
         int c1 = 0, c2 = 0;
 
-        ArrayList<Party> testWinner = new ArrayList<>();
-        testWinner.add(new Party("Democratic", 5, new ArrayList<>()));
-        testWinner.add(new Party("Republican", 5, new ArrayList<>()));
-
-        CPL cpl = new CPL(totalVotes, totalSeats, parties, br);
-        Party winner;
+        Election election = new Election(totalVotes, totalSeats, parties, br);
 
         for(int i = 0; i < 1000; i++) {
-            int index = cpl.coinToss(testWinner.size());
-            winner = testWinner.get(index);
-            if(winner.getName().equals("Democratic")) c1++;
+            int index = election.coinToss(2);
+
+            if(index == 0) c1++;
             else c2++;
         }
         System.out.println("Round 1");
@@ -38,13 +33,12 @@ public class TestCoinToss {
         c1 = 0;
         c2 = 0;
         int c3 = 0;
-        testWinner.add(new Party("Reform", 5, new ArrayList<>()));
         
         for(int i = 0; i < 1000; i++) {
-            int index = cpl.coinToss(testWinner.size());
-            winner = testWinner.get(index);
-            if(winner.getName().equals("Democratic")) c1++;
-            else if (winner.getName().equals("Republican")) c2++;
+            int index = election.coinToss(3);
+
+            if(index == 0) c1++;
+            else if (index == 1) c2++;
             else c3++;
         }
         System.out.println("Round 2");
@@ -53,57 +47,8 @@ public class TestCoinToss {
         System.out.println("c3: " + c3);
     }
 
-    public static void testCoinTossOPL() throws FileNotFoundException {
-        int totalVotes = 10000;
-        int totalSeats = 3;
-        ArrayList<Party> parties = new ArrayList<>();
-        FileReader fileReader = new FileReader("./testing/testOPLVote.csv");
-        BufferedReader br = new BufferedReader(fileReader);
-        ArrayList<Candidate> candidates = new ArrayList<>();
-        int c1 = 0, c2 = 0;
-
-        ArrayList<Candidate> testWinner = new ArrayList<>();
-        testWinner.add(new Candidate("Sara", "Democratic", 10));
-        testWinner.add(new Candidate("Bob Mc'Bobson", "Republican", 10));
-
-        OPL opl = new OPL(totalVotes, totalSeats, parties, br, candidates);
-        Candidate winner;
-
-        for(int i = 0; i < 1000; i++) {
-            int index = opl.coinToss(testWinner.size());
-            winner = testWinner.get(index);
-            if(winner.getName().equals("Sara")) c1++;
-            else c2++;
-        }
-        System.out.println("Round 1");
-        System.out.println("c1: " + c1);
-        System.out.println("c2: " + c2 + "\n");
-
-        c1 = 0; 
-        c2 = 0;
-        int c3 = 0, c4 = 0;
-        testWinner.add(new Candidate("Steve Mc'Steveson", "Green", 10));
-        testWinner.add(new Candidate("Renee", "Independent", 10));
-
-        for(int i = 0; i < 1000; i++) {
-            int index = opl.coinToss(testWinner.size());
-            winner = testWinner.get(index);
-            if(winner.getName().equals("Sara")) c1++;
-            else if(winner.getName().equals("Bob Mc'Bobson")) c2++;
-            else if(winner.getName().equals("Steve Mc'Steveson")) c3++;
-            else c4++;
-        }
-        System.out.println("Round 2");
-        System.out.println("c1: " + c1);
-        System.out.println("c2: " + c2);
-        System.out.println("c3: " + c3);
-        System.out.println("c4: " + c4);
-    }
 
     public static void main (String[] args) throws FileNotFoundException{
-        testCoinTossCPL();
-
-        System.out.println("\n\nTesting coinToss OPL");
-        testCoinTossOPL();
+        testCoinToss();
     }    
 }
